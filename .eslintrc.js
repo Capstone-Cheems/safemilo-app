71b1521275fa7930e5770302d0923022f5ca573a
Uma Maheswara Rao <uvoleti00@mylangara.ca>
2025-02-11 08:03:50 -0800

    refactor: upated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,20 @@
-// https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
-  ignorePatterns: ['/dist/*'],
-};
+    root: true,
+    extends: [
+        'expo',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'prettier/prettier': 'error'
+    }
+}
