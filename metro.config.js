b826ad0e2e0fb1bd8164cc827401ecc58edaeae6
ccyeung <ccyeung22@gmail.com>
2025-02-23 11:34:03 -0800

    update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,19 @@
 // metro.config.js
-const { getDefaultConfig } = require('@expo/metro-config');
+import { getDefaultConfig } from '@expo/metro-config'
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
-const { transformer, resolver } = config;
+const { transformer, resolver } = config
 
 config.transformer = {
     ...transformer,
     babelTransformerPath: require.resolve('react-native-svg-transformer')
-};
+}
 
 config.resolver = {
     ...resolver,
     assetExts: resolver.assetExts.filter(ext => ext !== 'svg'),
     sourceExts: [...resolver.sourceExts, 'svg', 'png', 'cjs']
-};
+}
 
-module.exports = config;
+export default config
