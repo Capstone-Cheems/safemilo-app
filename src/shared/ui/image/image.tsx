a21949fe6bdef043e6b579690f53e7e381d5d364
ccyeung <ccyeung22@gmail.com>
2025-03-13 22:03:32 -0700

    add ImagePicker

--- src/shared/ui/image/image.tsx
+++ src/shared/ui/image/image.tsx
@@ -1,30 +1,30 @@
 import React, { ComponentPropsWithRef, useState } from 'react'
 import Constants from 'expo-constants'
-import { Image, ImageSourcePropType } from 'react-native'
-import { scamTypeImages, DEFAULT_SCAM_IMAGE } from './scam-news-image'
+import { Image } from 'react-native'
 
 export const ImageView: React.FC<
     {
         coverImage: string
-        scamType?: string
     } & ComponentPropsWithRef<typeof Image>
-> = ({ coverImage, scamType, ...rest }) => {
+> = ({ coverImage, ...rest }) => {
     const imageURL = Constants.expoConfig?.extra?.S3_IMAGE_URL ?? ''
     const noImage =
         Constants.expoConfig?.extra?.S3_NO_IMAGE ?? 'default-placeholder.jpg'
-    const [imageSource, setImageSource] = useState<ImageSourcePropType>(
-        scamType
-            ? scamTypeImages[scamType] || DEFAULT_SCAM_IMAGE
-            : { uri: `${imageURL}${coverImage}` }
-    )
+    const [imageSource, setImageSource] = useState(`${imageURL}${coverImage}`)
     const [hasError, setHasError] = useState(false)
 
     const handleImageError = (): void => {
         if (!hasError) {
-            setImageSource({ uri: `${imageURL}${noImage}` })
+            setImageSource(`${imageURL}${noImage}`)
             setHasError(true) // Prevent infinite loop
         }
     }
 
-    return <Image source={imageSource} onError={handleImageError} {...rest} />
+    return (
+        <Image
+            source={{ uri: imageSource }}
+            onError={handleImageError}
+            {...rest}
+        />
+    )
 }
