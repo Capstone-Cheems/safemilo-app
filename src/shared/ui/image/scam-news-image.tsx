985212f92551337e78393615263054512b35b68e
ccyeung <ccyeung22@gmail.com>
2025-03-28 11:44:30 -0700

    add images

--- src/shared/ui/image/scam-news-image.tsx
+++ src/shared/ui/image/scam-news-image.tsx
@@ -7,7 +7,9 @@ export const scamTypeImages: Record<string, ImageSourcePropType> = {
     'Government Fraud': require('@/assets/images/government-fraud.jpg'),
     'Phone Scam': require('@/assets/images/phone-scam.jpg'),
     'Shopping Scam': require('@/assets/images/shopping-scam.jpg'),
-    'Financial Scam': require('@/assets/images/financial-scam.jpg')
+    'Financial Scam': require('@/assets/images/financial-scam.jpg'),
+    'Parking Scam': require('@/assets/images/parking-scam.jpg'),
+    'Package Delivery Scam': require('@/assets/images/package-delivery-scam.jpg')
 }
 
 export const DEFAULT_SCAM_IMAGE: ImageSourcePropType = require('@/assets/images/default-scam.jpg')
